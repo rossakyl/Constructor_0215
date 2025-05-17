2a13752a30a5a940177c927e2a8f76f6e750f008
rossakyl <rossakayla01@gmail.com>
2025-05-17 15:08:35 +0700

    Menambahkan class angka untuk menyimpan array dinamis dan menampilkan isinya

---
+++ destruktor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// Menambahkan class angka untuk menyimpan array dinamis dan menampilkan isinya
+class angka {
+private:
+    int *arr;
+    int panjang;
+
+};
