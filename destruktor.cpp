79bd8d5863bcfffec7392a827b11dd1cac3c13af
rossakyl <rossakayla01@gmail.com>
2025-05-17 15:10:20 +0700

    tambahkan konstruktor dan destruktor untuk class angka

--- destruktor.cpp
+++ destruktor.cpp
@@ -6,5 +6,9 @@ class angka {
 private:
     int *arr;
     int panjang;
-
+public:
+    angka(int);    // Constructor: mengatur panjang dan alokasi array
+    ~angka();      // Destructor: dealokasi array dan memberi pesan
+    void cetakData();  // Fungsi untuk mencetak isi array
+    void isiData();    // Fungsi untuk mengisi array dari input
 };
