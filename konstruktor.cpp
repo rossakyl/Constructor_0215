9c31c241d3b5c6aa9508b115e03b09971a408abf
rossakyl <rossakayla01@gmail.com>
2025-05-17 15:35:40 +0700

    implementasikan pemanggilan constructor overload di main

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -31,3 +31,21 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
     nim = iNim;
     nama = iNama;
 }
+void mahasiswa::cetak() {  // Method cetak() digunakan untuk menampilkan data nim dan nama dari objek mahasiswa
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs1;                  // Menggunakan constructor default
+    mahasiswa mhs2(20);              // Menggunakan constructor dengan int
+    mahasiswa mhs3("Indra");         // Menggunakan constructor dengan string
+    mahasiswa mhs4(30, "Fauzan");    // Menggunakan constructor dengan int dan string
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
