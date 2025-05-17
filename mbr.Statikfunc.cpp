56617cbb2cc02999a7b112d7dcfebacc69ad6991
rossakyl <rossakayla01@gmail.com>
2025-05-17 15:46:24 +0700

    implementasi class mahasiswa dengan static member dan static functions untuk ID unik

--- mbr.Statikfunc.cpp
+++ mbr.Statikfunc.cpp
@@ -18,3 +18,31 @@ public:
     mahasiswa(string pnama) : nama(pnama) { setID(); } // Constructor dengan parameter nama
 };
 
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() { // Fungsi untuk memberikan nilai ID unik ke setiap objek mahasiswa
+    id = ++nim; // ID akan selalu bertambah seiring objek baru dibuat
+}
+
+void mahasiswa::printAll() { // Fungsi untuk menampilkan informasi mahasiswa
+    cout << "ID     = " << id << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    mahasiswa mhs1("Sri Dadi");           // ID: 1
+    mahasiswa mhs2("Budi Jatmiko");       // ID: 2
+    mahasiswa::setNim(9); // Nim di-reset ke 9 (ID berikutnya jadi 10)
+    mahasiswa mhs3("Andi Janu");          // ID: 10
+    mahasiswa mhs4("Joko Wahono");        // ID: 11
+
+    mhs1.printAll(); // Menampilkan semua data mahasiswa
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar object = " << mahasiswa::getNim() << endl; // Menampilkan nilai static nim terakhir dari luar objek
+
+    return 0;
+}
