616b7181b3a25365c16af6f03e2346824db9ef49
rossakyl <rossakayla01@gmail.com>
2025-05-17 15:25:45 +0700

    Menambahkan class mahasiswa dengan atribut nim dan nama serta constructor overload

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();                             // Constructor default
+    mahasiswa(int);                          // Constructor dengan parameter int
+    mahasiswa(string);                       // Constructor dengan parameter string
+    mahasiswa(int iNim, string iNama);       // Constructor dengan parameter int dan string
+    void cetak();                            // Method untuk mencetak data
+};
